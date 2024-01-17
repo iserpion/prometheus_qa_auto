c730fd49042956bf33362fb3802d803e409f8155
IvanSerpionov <iserpionov@gmail.com>
2024-01-17 15:59:20 +0200

    Added required tests for Database module

---
+++ modules/common/database.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+
+class Database():
+
+    def __init__(self):
+        self.connection = sqlite3.connect(r'C:\\Users\\iserp\\qa_auto\\prometheus_qa_auto' + r'\\become_qa_auto.db')
+        self.cursor = self.connection.cursor()
+
+    def test_connection(self):
+        sqlite_select_query = 'SELECT sqlite_version();'
+        self.cursor.execute(sqlite_select_query)
+        record = self.cursor.fetchall()
+        print(f'Connected successfully. SQLite Database Version is: {record}')
+
+    def get_all_users(self):
+        query = 'SELECT name, address, city FROM customers'
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+    
+    def get_user_address_by_name(self, name):
+        query = f"SELECT address, city, postalCode, country FROM customers WHERE name = '{name}'"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+
+    def update_product_qnt_by_id(self, product_id, qnt):
+        query = f"UPDATE products SET quantity = {qnt} WHERE id = {product_id}"
+        self.cursor.execute(query)
+        self.connection.commit()
+    
+    def select_product_qnt_by_id(self, product_id):
+        query = f"SELECT quantity FROM products WHERE id = {product_id}"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+    
+    def insert_product(self, product_id, name, description, qnt):
+        query = f"INSERT OR REPLACE INTO products (id, name, description, quantity) \
+            VALUES ({product_id}, '{name}', '{description}', {qnt})"
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def delete_product_by_id(self, product_id):
+        query = f"DELETE FROM products WHERE id = {product_id}"
+        self.cursor.execute(query)
+        self.connection.commit()
+    
+    def get_detailed_orders(self):
+        query = "SELECT o.id, c.name, p.name, \
+                p.description, o.order_date \
+                FROM orders as o \
+                JOIN customers as c ON o.customer_id = c.id \
+                JOIN products as p ON o.product_id = p.id"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
