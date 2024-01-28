73a303ab8f1edabe43457cced7afa8d1650064ef
IvanSerpionov <iserpionov@gmail.com>
2024-01-28 16:41:58 +0200

    trivial changes in test files formatting

--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -1,9 +1,11 @@
 import pytest
 
+
 # Required part of project:
 @pytest.mark.database
 def test_database_connection(db):
     db.test_connection()
+
 
 @pytest.mark.database
 def test_check_all_users(db):
@@ -11,14 +13,16 @@ def test_check_all_users(db):
 
     print(users)
 
+
 @pytest.mark.database
 def test_check_user_sergii(db):
-    user = db.get_user_address_by_name('Sergii')
+    user = db.get_user_address_by_name("Sergii")
 
-    assert user[0][0] == 'Maydan Nezalezhnosti 1'
-    assert user[0][1] == 'Kyiv'
-    assert user[0][2] == '3127'
-    assert user[0][3] == 'Ukraine'
+    assert user[0][0] == "Maydan Nezalezhnosti 1"
+    assert user[0][1] == "Kyiv"
+    assert user[0][2] == "3127"
+    assert user[0][3] == "Ukraine"
+
 
 @pytest.mark.database
 def test_product_qnt_update(db):
@@ -27,54 +31,59 @@ def test_product_qnt_update(db):
 
     assert water_qnt[0][0] == 25
 
+
 @pytest.mark.database
 def test_product_insert(db):
-    db.insert_product(4, 'печиво', 'солодке', 30)
+    db.insert_product(4, "печиво", "солодке", 30)
     cookies_qnt = db.select_product_qnt_by_id(4)
 
     assert cookies_qnt[0][0] == 30
 
+
 @pytest.mark.database
 def test_product_delete(db):
-    db.insert_product(99, 'тестові', 'дані', 999)
+    db.insert_product(99, "тестові", "дані", 999)
     db.delete_product_by_id(99)
     qnt = db.select_product_qnt_by_id(99)
 
     assert len(qnt) == 0
 
+
 @pytest.mark.database
 def test_detailed_orders(db):
     orders = db.get_detailed_orders()
-    print('Замовлення', orders)
+    print("Замовлення", orders)
     # Check quantity of orders equal to 1
     assert len(orders) == 1
 
     # Check structure of data
     assert orders[0][0] == 1
-    assert orders[0][1] == 'Sergii'
-    assert orders[0][2] == 'солодка вода'
-    assert orders[0][3] == 'з цукром'
+    assert orders[0][1] == "Sergii"
+    assert orders[0][2] == "солодка вода"
+    assert orders[0][3] == "з цукром"
+
 
 # Individual part of project:
 @pytest.mark.database
 def test_insert_duplicated_key(db):
     """Test verifies that it is unable to insert data
-      with duplicated primary key to products table"""
-    
-    db.pure_insert_product(4, 'cookie', 'sweet', 30)
+    with duplicated primary key to products table"""
+
+    db.pure_insert_product(4, "cookie", "sweet", 30)
     product_count = db.select_count_product_by_id(4)
     # check that product is not duplicated
     assert product_count[0][0] == 1
+
 
 @pytest.mark.database
 def test_create_table_and_manipulate_with_it(db):
     """Test verifies that a table can be created,
     some data can be stored/read to/from it and table can be dropped from DB"""
-    
+
     db.create_table_order_details()
     table_names = db.select_name_from_sqlite_schema()
     # check that table is created
-    assert table_names[3][0] == 'order_details'
+    assert table_names[3][0] == "order_details"
 
     db.insert_order_details(1, 1, 5)
     order_detail = db.select_order_details()
@@ -88,14 +97,11 @@ def test_create_table_and_manipulate_with_it(db):
     # check that table is deleted
     assert len(table_names) == 3
 
+
 @pytest.mark.database
 @pytest.mark.parametrize(
-    'id, name, description, quantity', 
-    [
-        (5, 'meat', 'chicken', 9),
-        (6, 'beer', 'lager', 24),
-        (7, 'juice', 'banana', 14)
-    ]
+    "id, name, description, quantity",
+    [(5, "meat", "chicken", 9), (6, "beer", "lager", 24), (7, "juice", "banana", 14)],
 )
 def test_insert_products_multiple_times(db, id, name, description, quantity):
     """Test that inserts data into products table multiple times"""
@@ -103,39 +109,44 @@ def test_insert_products_multiple_times(db, id, name, description, quantity):
     db.insert_product(id, name, description, quantity)
     qnt = db.select_product_qnt_by_id(id)
     # check that data inserted
-    assert qnt[0][0] == quantity   
+    assert qnt[0][0] == quantity
+
 
 @pytest.mark.database
 def test_min_product_qty(db):
     """Test checks minimum quantity of products"""
 
-    minimum_qty = db.select_min_max_avg_product_qty('min')
+    minimum_qty = db.select_min_max_avg_product_qty("min")
     # check that minimum quantity is valid
     assert minimum_qty[0][0] == 9
+
 
 @pytest.mark.database
 def test_max_product_qty(db):
     """Test checks maximum quantity of products"""
 
-    maximum_qty = db.select_min_max_avg_product_qty('max')
+    maximum_qty = db.select_min_max_avg_product_qty("max")
     # check that maximum quantity is valid
     assert maximum_qty[0][0] == 30
+
 
 @pytest.mark.database
 def test_avg_product_qty(db):
     """Test checks average quantity of products"""
 
-    average_qty = db.select_min_max_avg_product_qty('avg')
+    average_qty = db.select_min_max_avg_product_qty("avg")
     # check that average quantity is valid
     assert average_qty[0][0] == 17
+
 
 @pytest.mark.database
 def test_max_product_description(db):
     """Test checks product description with maximum length"""
 
     max_desc = db.select_product_with_max_description()
-    # check that product description with maximum length is valid 
-    assert max_desc[0][0] == 'натуральне незбиране'
+    # check that product description with maximum length is valid
+    assert max_desc[0][0] == "натуральне незбиране"
+
 
 @pytest.mark.database
 def test_max_city_count_in_customers(db):
@@ -143,11 +154,12 @@ def test_max_city_count_in_customers(db):
 
     max_city = db.select_customers_city_max_count()
     # check the city is valid
-    assert max_city[0][0] == 'Odesa'
+    assert max_city[0][0] == "Odesa"
+
 
 @pytest.mark.database
 def test_best_selling_product(insert_delete_orders):
     """Test checks the best selling product"""
 
     bestseller = insert_delete_orders.select_bestseller()
-    assert bestseller[0][0] == 'beer'
+    assert bestseller[0][0] == "beer"
--- tests/ui/test_ui_page_object.py
+++ tests/ui/test_ui_page_object.py
@@ -4,7 +4,8 @@ from modules.ui.page_objects.sign_in_page import SignInPage
 
 # Required part of project:
 
-@pytest.mark.ui 
+
+@pytest.mark.ui
 def test_check_incorrect_username_page_object():
     # Create page object
     sign_in_page = SignInPage()
@@ -13,10 +14,10 @@ def test_check_incorrect_username_page_object():
     sign_in_page.go_to()
 
     # Attempt to login in GitHub with wrong credentials
-    sign_in_page.try_login('page_object@gmail.com', 'wrong password')
+    sign_in_page.try_login("page_object@gmail.com", "wrong password")
 
     # Check that page title is as expected
-    assert sign_in_page.check_title('Sign in to GitHub · GitHub')
+    assert sign_in_page.check_title("Sign in to GitHub · GitHub")
 
     # Close browser
     sign_in_page.close()
@@ -24,19 +25,22 @@ def test_check_incorrect_username_page_object():
 
 # Individual part of project:
 
+
 @pytest.mark.ui
 def test_check_allo_product_is_added_to_cart(allo_page):
     """Test checks adding top product to cart from allo.ua main page"""
 
     allo_page.add_product_to_cart()
 
-    expected_cart_popup_title = 'Кошик'
+    expected_cart_popup_title = "Кошик"
 
     # Step 1: check that cart popup is opened
     assert allo_page.cart_data["popup_title"] == expected_cart_popup_title
 
     # Step 2: check that product title in cart popup is the same as on main page
-    assert allo_page.product_data["product_title"] == allo_page.cart_data["product_title"]
+    assert (
+        allo_page.product_data["product_title"] == allo_page.cart_data["product_title"]
+    )
 
     product_price = allo_page.get_nums_from_str(allo_page.product_data["product_price"])
     cart_price = allo_page.get_nums_from_str(allo_page.cart_data["product_price"])
@@ -48,34 +52,35 @@ def test_check_allo_product_is_added_to_cart(allo_page):
     # Step 4: check that cart total price is the same as product price on main page
     assert total_price == product_price
 
-@pytest.mark.ui 
+
+@pytest.mark.ui
 def test_check_allo_product_is_removed_from_cart(allo_page):
     """Test checks removing product from cart on allo.ua"""
 
     allo_page.add_product_to_cart()
     allo_page.remove_product_from_cart()
 
-    expected_empty_cart_text = 'Ваш кошик порожній.'
+    expected_empty_cart_text = "Ваш кошик порожній."
 
     # check that product is removed
     assert allo_page.empty_cart_msg == expected_empty_cart_text
 
 
-@pytest.mark.ui 
+@pytest.mark.ui
 def test_check_allo_change_product_qty_in_cart(allo_page):
     """Test checks changing product quantity in cart popup"""
 
     allo_page.add_product_to_cart()
     allo_page.increase_product_qty_in_cart()
     # print(allo_page.increase_data)
-    
-    increase_result = allo_page.validate_increase_decrease('increase')
+
+    increase_result = allo_page.validate_increase_decrease("increase")
 
     # Step 1: check that qty of products is increased(product price x3)
     assert increase_result
 
     allo_page.decrease_product_qty_in_cart()
-    decrease_result = allo_page.validate_increase_decrease('decrease')
+    decrease_result = allo_page.validate_increase_decrease("decrease")
 
     # Step 2: check that qty of products is decreased(product price x2)
     assert decrease_result
@@ -87,33 +92,34 @@ def test_check_novaposhta_delivery_cost(nova_page):
 
     nova_page.fill_fields()
     nova_page.calculate_delivery()
-    expected_text = 'Вартість перевезення 205.00 ... 210.00грн'
+    expected_text = "Вартість перевезення 205.00 ... 210.00грн"
 
     # check that calculated cost is shown
     assert nova_page.calculated_cost == expected_text
+
 
 @pytest.mark.ui
 def test_check_carid_login(carid_login):
     """Test checks login to carid.com"""
 
     carid_login.login()
-    expected_url = 'https://www.carid.com/my-account/'
+    expected_url = "https://www.carid.com/my-account/"
 
     # check that new user is logged in
     assert carid_login.current_url == expected_url
 
+
 @pytest.mark.ui
 def test_check_carid_add_ship_address(carid_my_account):
-        
     carid_my_account.add_shipping_address()
 
     # check that shipping address info is added to My Account page
-    assert carid_my_account.validate_address_info('shipping')
+    assert carid_my_account.validate_address_info("shipping")
+
 
 @pytest.mark.ui
 def test_check_carid_add_bill_address(carid_my_account):
-        
     carid_my_account.add_billing_address()
 
     # check that billing address info is added to My Account page
-    assert carid_my_account.validate_address_info('billing')
+    assert carid_my_account.validate_address_info("billing")
