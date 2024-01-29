f4ada5f639ab00c8979aad1c3cf2f0c2651019fd
IvanSerpionov <iserpionov@gmail.com>
2024-01-29 19:14:22 +0200

    Minor changes in address_generator.py

--- modules/common/generators/address_generator.py
+++ modules/common/generators/address_generator.py
@@ -2,7 +2,7 @@ from modules.common.data.address import Address
 from faker import Faker
 
 
-faker_en = Faker('en-US')
+faker_en = Faker("en-US")
 
 
 def generated_shipping_address():
@@ -12,23 +12,24 @@ def generated_shipping_address():
         company=faker_en.company(),
         address_line_1=faker_en.street_address(),
         address_line_2=faker_en.secondary_address(),
-        city='Montgomery',
-        state='Alabama',
-        country='United States',
-        zipcode='36107',
-        phone=faker_en.phone_number()
+        city="Montgomery",
+        state="Alabama",
+        country="United States",
+        zipcode="36107",
+        phone=faker_en.phone_number(),
     )
 
+
 def generated_billing_address():
-        return Address(
+    return Address(
         first_name=faker_en.first_name(),
         last_name=faker_en.last_name(),
         company=faker_en.company(),
         address_line_1=faker_en.street_address(),
         address_line_2=faker_en.secondary_address(),
-        city='Hanceville',
-        state='Alabama',
-        country='United States',
-        zipcode='35077',
-        phone=faker_en.phone_number()
+        city="Hanceville",
+        state="Alabama",
+        country="United States",
+        zipcode="35077",
+        phone=faker_en.phone_number(),
     )
